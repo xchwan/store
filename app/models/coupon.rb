cafd1474b8a3d7658a21c332c91ccb8ca7249bf5
Chao-Hung, Wan <qchwan@gmail.com>
2022-06-19 18:47:49 +0800

    set up coupon code generate

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -2,5 +2,14 @@ class Coupon < ApplicationRecord
   has_many :books
   validates :code, uniqueness: true
   validates :discount, presence: true, numericality: {greater_than: 0}
+  before_save :generate_code
 
+  private
+    def generate_code
+      if self.code.empty?
+        self.code = SecureRandom.hex[1..8].upcase
+      else
+        self.code = code.upcase
+      end
+    end
 end
