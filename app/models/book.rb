96b470ff7aa18941c68621421b87054a6240813f
Chao-Hung, Wan <qchwan@gmail.com>
2022-06-19 16:49:46 +0800

    add relation between coupon and books

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+  belongs_to :coupon
+  validates :title, presence: true
+  validates :price, presence: true, numericality: {greater_than: 0}
 end
--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,2 +1,6 @@
 class Coupon < ApplicationRecord
+  has_many :books
+  validates :code, uniqueness: true
+  validates :discount, presence: true, numericality: {greater_than: 0}
+
 end
